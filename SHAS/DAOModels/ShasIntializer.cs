1b36d95f3efe608ad883f0e5b3ea3340e9359bf5
kodeBugger <nirmalutd@gmail.com>
2013-04-15 23:18:47 -0500

    DAO Models

---
+++ SHAS/DAOModels/ShasIntializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace SHAS.DAOModels
+{
+    public class ShasIntializer : DropCreateDatabaseIfModelChanges<Context>
+    {
+        protected override void Seed(Context context)
+        {
+            GetUserLoginDao().ForEach(c => context.UserLoginDAO.Add(c));
+           
+        }
+        private static List<UserLoginDAO> GetUserLoginDao()
+        {
+            var userLogin = new List<UserLoginDAO>{
+                new UserLoginDAO{
+
+                    userName ="Admin",
+                    Password ="Password123"
+                }
+                };
+
+            return userLogin;
+        
+        
+        }
+
+
+    }
+}
